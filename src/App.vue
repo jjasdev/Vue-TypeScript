76ef80c7ac9407f017ce108431736995ca7bd472
juanjoalsz <juanjoalsz@gmail.com>
2023-01-17 00:36:33 +0100

    Final proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,67 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, reactive } from "vue";
+import Job from "./types/Job";
+import JobList from "./components/JobsList.vue";
+import OrderTerm from "./types/OrderTerms";
+
+const name = ref("Juanjo");
+const age = ref<string | number>(37);
+const user = reactive({
+  name: "Juanjo",
+  age: 37 as string | number,
+});
+const jobs = ref<Job[]>([
+  { title: "programmer", location: "Spain", salary: 20000, id: "1" },
+  { title: "maths", location: "Portugal", salary: 40000, id: "2" },
+  { title: "police", location: "France", salary: 30000, id: "3" },
+]);
+const order = ref<OrderTerm>("title");
+function changeName(newName: string) {
+  name.value = newName;
+}
+function changeAge(newAge: number | string) {
+  age.value = newAge;
+}
+function handleClick(term: OrderTerm) {
+  order.value = term;
+}
+</script>
 
 <template>
-  <div>Hello Juanjo</div>
+  <div>Hello {{ name }} - {{ age }}</div>
+  <button @click="changeName('Elena')">Change name</button>
+  <button @click="changeAge(32)">Change age</button>
+  <p>{{ jobs[0].location }}</p>
+  <div class="app">
+    <header>
+      <div class="title">
+        <h1>Jobs</h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">Order by title</button>
+        <button @click="handleClick('salary')">Order by salary</button>
+        <button @click="handleClick('location')">Order by location</button>
+      </div>
+    </header>
+    <JobList :jobs="jobs" :order="order" />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/JobsList.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import { toRefs, computed } from "vue";
+import Job from "../types/Job";
+import OrderTerm from "../types/OrderTerms";
+
+interface Props {
+  jobs: Job[];
+  order: OrderTerm;
+}
+const props = withDefaults(defineProps<Props>(), {
+  jobs: () => [],
+});
+const { jobs } = toRefs(props);
+const orderedJobs = computed(() => {
+  return [...props.jobs].sort((a: Job, b: Job) => {
+    return a[props.order] > b[props.order] ? 1 : -1;
+  });
+});
+</script>
+<template>
+  <div class="job-list">
+    <h3>Ordered by {{ order }}</h3>
+    <transition-group name="list" tag="ul">
+      <li v-for="job in orderedJobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <p>{{ job.salary }} rupees</p>
+        </div>
+        <div class="description">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem omnis
+            voluptatum eius doloremque optio iusto sequi dignissimos. Pariatur
+            earum assumenda dolores possimus quidem quam, reprehenderit aliquid
+            consequuntur amet non facere.
+          </p>
+        </div>
+      </li>
+    </transition-group>
+  </div>
+</template>
+
+<style scoped>
+.job-list {
+  max-width: 960px;
+  margin: 40px auto;
+}
+.job-list ul {
+  padding: 0;
+}
+.job-list li {
+  list-style-type: none;
+  background: white;
+  padding: 16px;
+  margin: 16px 0;
+  border-radius: 4px;
+}
+.job-list h2 {
+  margin: 0 0 10px;
+  text-transform: capitalize;
+}
+.salary {
+  display: flex;
+}
+.salary img {
+  width: 30px;
+}
+.salary p {
+  color: #17bf66;
+  font-weight: bold;
+  margin: 10px 4px;
+}
+h2,
+p {
+  color: black;
+}
+.list-move {
+  transition: all 1s;
+}
+</style>
